8394c90478e1935a7a8002206c442087aad94ab6
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-02 18:01:00 +0100

    added separate views pages for calculator

--- calc_it_sinatra/app.rb
+++ calc_it_sinatra/app.rb
@@ -5,3 +5,23 @@ get "/" do
   @title = "Welcome to Calc It"
   erb(:home)
 end 
+
+get "/basic" do
+  @title = "Welcome to Basic Calculator"
+  erb(:basic)
+end 
+
+get "/mortgage" do
+  @title = "Welcome to Mortgage Calculator"
+  erb(:mortgage)
+end 
+
+get "/bmi" do
+  @title = "Welcome to BMI Calculator"
+  erb(:bmi)
+end 
+
+get "/distance" do
+  @title = "Welcome to Distance Calculator"
+  erb(:distance)
+end 
